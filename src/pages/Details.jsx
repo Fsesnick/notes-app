f19a88833f380e18383309927466bcf556ea4e0c
sesnick <fernanda.sesnick@vtex.com.br>
2023-12-15 10:05:59 -0300

    fist test

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+export function Details(){
+  
+  return(
+    <Fragment>
+       <h1>Hello world</h1>
+       <span>Fernanda Sesnick</span>
+    </Fragment>
+   
+  )
+}
