43cc74d7acb81a2e97b68424e1d17bb45a0e6e65
sesnick <fernanda.sesnick@vtex.com.br>
2023-12-26 15:33:45 -0300

    secoes pagida details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,8 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 
 export function Details(){
@@ -8,7 +10,16 @@ export function Details(){
   return(
     <Container>
       <Header/>
-      
+      <Section title="Links uteis">
+      <Links> 
+        <li><a href="#">https://www.rocketseat.com.br</a></li>
+        <li> <a href="#">https://www.rocketseat.com.br</a> </li>
+      </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title="express" />
+      </Section>
+
        <Button title= "Voltar"/>
     </Container>
    
