620e853ddbde9903849d29a13bee2570e281472e
sesnick <fernanda.sesnick@vtex.com.br>
2024-01-04 18:19:33 -0300

    criacao da pagina Profile

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { SignIn } from './pages/SignIn'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
+import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/> 
-    <SignIn />
+    <Profile />
     </ThemeProvider>
     
   </React.StrictMode>,
